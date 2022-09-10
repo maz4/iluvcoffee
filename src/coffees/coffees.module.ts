30636327977cb2fbba0d40c0552f6db49b70746b
Marcin Zasadzki <zasadzkimarcin@gmail.com>
2022-09-10 19:58:09 +0100

    inject custom async provider

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,5 +1,6 @@
 import { Injectable, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 import { EventSchema } from 'src/events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
 import { CoffeesController } from './coffees.controller';
@@ -34,18 +35,34 @@ export class CoffeeBrandsFactory {
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
-    CoffeeBrandsFactory,
-    {
-      provide: ConfigService,
-      useClass:
-        process.env.NODE_ENV === 'development'
-          ? DevelopmentConfigService
-          : ProductionConfigService,
-    },
+    Connection,
+    // CoffeeBrandsFactory,
+    // {
+    //   provide: ConfigService,
+    //   useClass:
+    //     process.env.NODE_ENV === 'development'
+    //       ? DevelopmentConfigService
+    //       : ProductionConfigService,
+    // },
+    // {
+    //   provide: COFFEE_BRANDS,
+    //   useFactory: (brandsFactory) => brandsFactory.create(),
+    //   inject: [CoffeeBrandsFactory],
+    // },
     {
       provide: COFFEE_BRANDS,
-      useFactory: (brandsFactory) => brandsFactory.create(),
-      inject: [CoffeeBrandsFactory],
+      useFactory: async (connection: Connection): Promise<string[]> => {
+        // const coffeeBrands = await connection.query('SELECT * ...');
+        const coffeeBrands = await Promise.resolve([
+          'buddy brew',
+          'nescafe',
+          'promise brew',
+        ]);
+
+        console.log('[!] Async factory');
+        return coffeeBrands;
+      },
+      inject: [Connection],
     },
   ],
   exports: [CoffeesService],
