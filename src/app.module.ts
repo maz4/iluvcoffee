4058b12e9769db0dd2c0291def2609ae4350dc57
Marcin Zasadzki <zasadzkimarcin@gmail.com>
2022-09-11 10:49:01 +0100

    add env file and @hapi/joi validatior

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,25 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_URI: Joi.required(),
+        DATABASE_PORT: Joi.string().default('27017'),
+      }),
+    }),
     CoffeesModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-course'),
+    // MongooseModule.forRoot('mongodb://localhost:27017/nest-course'), - old version
+    MongooseModule.forRoot(
+      `${process.env.DATABASE_URI}:${process.env.DATABASE_PORT}`,
+      {
+        dbName: process.env.DATABASE_NAME,
+      },
+    ),
     CoffeeRatingModule,
     DatabaseModule,
   ],
